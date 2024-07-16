8c560562cfa6cff674ecf365eabec4c0e98a6228
annazeit <77589107+annazeit@users.noreply.github.com>
2024-07-16 14:27:10 +0100

    Delete all_prime directory

--- all_prime/target/debug/all_prime.d
+++
@@ -1,1 +0,0 @@
-C:\Users\annaz\Rover\all_prime\target\debug\all_prime.exe: C:\Users\annaz\Rover\all_prime\src\main.rs
--- all_prime/target/debug/deps/all_prime.d
+++
@@ -1,5 +0,0 @@
-C:\Users\annaz\Rover\all_prime\target\debug\deps\all_prime.exe: src/main.rs
-
-C:\Users\annaz\Rover\all_prime\target\debug\deps\all_prime.d: src/main.rs
-
-src/main.rs:
