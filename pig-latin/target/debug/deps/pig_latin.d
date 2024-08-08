bfcaf810db1248de8bd5a77209a90165f24938fe
annazeit <77589107+annazeit@users.noreply.github.com>
2024-08-08 16:10:55 +0100

    Delete pig-latin/target directory

--- pig-latin/target/debug/deps/pig_latin.d
+++
@@ -1,5 +0,0 @@
-C:\Users\annaz\Rover\pig-latin\target\debug\deps\pig_latin.exe: src/main.rs
-
-C:\Users\annaz\Rover\pig-latin\target\debug\deps\pig_latin.d: src/main.rs
-
-src/main.rs:
--- pig-latin/target/debug/pig-latin.d
+++
@@ -1,1 +0,0 @@
-C:\Users\annaz\Rover\pig-latin\target\debug\pig-latin.exe: C:\Users\annaz\Rover\pig-latin\src\main.rs
